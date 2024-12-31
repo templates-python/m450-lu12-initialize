724452d1bcc69c25c693e7cb15bdb8df99f417ea
AndreProbst <76043233+AndreProbst@users.noreply.github.com>
2024-12-31 13:58:20 +0100

    Basiscode ergänzt

--- main_test.py
+++ main_test.py
@@ -8,6 +8,6 @@ def test(capsys):
     
     Test für Anzahl Kunden (100)
     Test für Anzahl Buchungen (1000)
-    Test auf die Summer der Buchungen (den Vergleichswert haben Sie im ersten Auftrag ermittelt)
+    Test auf die Summe der Buchungen (den Vergleichswert haben Sie im ersten Auftrag ermittelt)
     
     """
